ef92e4433fc211b78fba5165a9830ee57744e2c0
helenaczats <helenaczats@gmail.com>
2024-04-11 15:41:46 +0200

    Criei um script de Selenium com behave

--- test_selecionar_produto_swd.py
+++ test_selecionar_produto_swd.py
@@ -1,6 +1,15 @@
 # 1- Bibliotecas
+
+import pytest      #Framework de Teste de Unidade
+import time
+import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 # 2- Classe (opcional)
 class Teste_Produtos():
@@ -29,5 +38,43 @@ class Teste_Produtos():
         #confirma o preço da mochila
         assert self.driver.find_element(By.CSS_SELECTOR, ".inventory_item:nth-child(1) .inventory_item_price").text == (
             "$29.99"
-            )
+        )
+            
+        # incluir produto no carrinho
+        
+        self.driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click() #adiciona produto no carrinho
+    
+        
+        # entrar no carrinho
+        
+        self.driver.find_element(By.CSS_SELECTOR, "shopping-cart-link").click() #entra no carrinho de compras
+        
+        # validar carrinho de compras
+        
+        assert self.driver.find_element(By.CSS_SELECTOR, "shopping_cart_link").text == "1"
+       
+        # validar produto no carrinho
+        
+        assert self.driver.find_element(By.CSS_SELECTOR, "inventory_details_name large_size").text == "Sauce Labs Backpack" #valida nome mochila
+        assert self.driver.find_element(By.CSS_SELECTOR, "inventory_details_price").text == "$29.99" #valida preço mochila
+        
+        
+       # Remover o produto
+       
+        self.driver.find_element(By.CSS_SELECTOR, "btn btn_secondary btn_small btn_inventory").click() #remove mochila
+        
+       
+       # Realizar Logout
+        
+        self.driver.find_element(By.ID, "react-burger-menu-btn").click() #entra no menu
+   
+        self.driver.find_element(By.ID, "logout-sidebar-link").click() #faz log out no site
+        
+        time.sleep(5)
+        
+    
+    
+    
+   
+   
     
